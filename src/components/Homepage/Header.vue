273903b6c5d1aad41e8439a337a5194d4a75bae0
aingsopheak <aingsopheak13@gmail.com>
2023-03-23 09:22:31 +0700

    first commit

---
+++ src/components/Homepage/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header class="header">
+    <Navbar @toggle-menu-show="$emit('toggle-menu-show', $event)" />
+    <Hero />
+  </header>
+</template>
+
+<script>
+import Navbar from "../Navbar.vue";
+import Hero from "./Hero.vue";
+
+export default {
+  name: "Header",
+  components: { Navbar, Hero },
+  emits: ["toggle-menu-show"],
+  methods: {
+    clickHandler() {},
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  background-image: url("../../assets/Untitled1.jpg");
+  background-position: center;
+  background-size: cover;
+
+  @media (min-width: 768px) {
+    background-image: url("../../assets/Untitled1.jpg");
+    background-position: center;
+    background-size: cover;
+  }
+
+  @media (min-width: 1205px) {
+    background-image: url("../../assets/Untitled1.jpg");
+    background-size: cover;
+    background-position: center;
+  }
+}
+</style>
